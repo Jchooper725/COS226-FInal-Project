96a6c17cfa20f0e8f88509b55c5bcafe15359c81
Jchooper725 <jackson.hooper@maine.edu>
2025-12-18 20:32:09 -0500

    Indexing Implemented
    Added all the functions for the required functions associated with creating an index. This includes:
    Allow the user to set a field as an “index”.
    Creating an index builds a B+ tree on that field for range searching.
    Creating a new index does not discard other existing indexes
    Multiple indexes can exist simultaneously
    The interface should clearly indicate which fields are currently indexed

--- src/BTree.py
+++ src/BTree.py
@@ -1,202 +1,174 @@
-'''
-Created on Dec 10, 2025
 
-@author: jacksonhooper
-'''
-
-# Deleting a key on a B-tree in Python
-
-
-# Btree node
 class BTreeNode:
-    def __init__(self, leaf=False):
-        self.leaf = leaf
-        self.keys = []
-        self.child = []
+    def __init__(self, t, leaf=False):
+        self.t = t              # Minimum degree
+        self.leaf = leaf        # Is true when node is leaf
+        self.keys = []          # Keys in node
+        self.children = []      # Child pointers
+
+    def search(self, k):
+        i = 0
+        while i < len(self.keys) and k > self.keys[i]:
+            i += 1
+        if i < len(self.keys) and self.keys[i] == k:
+            return self
+        if self.leaf:
+            return None
+        return self.children[i].search(k)
+
+    def split_child(self, i, y):
+        t = self.t
+        z = BTreeNode(t, y.leaf)
+        mid_key = y.keys[t - 1]
+
+        z.keys = y.keys[t:]
+        y.keys = y.keys[:t - 1]
+
+        if not y.leaf:
+            z.children = y.children[t:]
+            y.children = y.children[:t]
+
+        self.children.insert(i + 1, z)
+        self.keys.insert(i, mid_key)
+
+    def insert_non_full(self, k):
+        i = len(self.keys) - 1
+        if self.leaf:
+            self.keys.append(None)
+            while i >= 0 and k < self.keys[i]:
+                self.keys[i + 1] = self.keys[i]
+                i -= 1
+            self.keys[i + 1] = k
+        else:
+            while i >= 0 and k < self.keys[i]:
+                i -= 1
+            i += 1
+            if len(self.children[i].keys) == 2 * self.t - 1:
+                self.split_child(i, self.children[i])
+                if k > self.keys[i]:
+                    i += 1
+            self.children[i].insert_non_full(k)
+
+    # -------- Deletion helpers --------
+    def remove(self, k):
+        idx = self.find_key(k)
+
+        if idx < len(self.keys) and self.keys[idx] == k:
+            if self.leaf:
+                self.keys.pop(idx)
+            else:
+                self.remove_from_non_leaf(idx)
+        else:
+            if self.leaf:
+                return
+            flag = idx == len(self.keys)
+            if len(self.children[idx].keys) < self.t:
+                self.fill(idx)
+            if flag and idx > len(self.keys):
+                self.children[idx - 1].remove(k)
+            else:
+                self.children[idx].remove(k)
+
+    def find_key(self, k):
+        idx = 0
+        while idx < len(self.keys) and self.keys[idx] < k:
+            idx += 1
+        return idx
+
+    def remove_from_non_leaf(self, idx):
+        k = self.keys[idx]
+        if len(self.children[idx].keys) >= self.t:
+            pred = self.get_pred(idx)
+            self.keys[idx] = pred
+            self.children[idx].remove(pred)
+        elif len(self.children[idx + 1].keys) >= self.t:
+            succ = self.get_succ(idx)
+            self.keys[idx] = succ
+            self.children[idx + 1].remove(succ)
+        else:
+            self.merge(idx)
+            self.children[idx].remove(k)
+
+    def get_pred(self, idx):
+        cur = self.children[idx]
+        while not cur.leaf:
+            cur = cur.children[-1]
+        return cur.keys[-1]
+
+    def get_succ(self, idx):
+        cur = self.children[idx + 1]
+        while not cur.leaf:
+            cur = cur.children[0]
+        return cur.keys[0]
+
+    def fill(self, idx):
+        if idx != 0 and len(self.children[idx - 1].keys) >= self.t:
+            self.borrow_from_prev(idx)
+        elif idx != len(self.keys) and len(self.children[idx + 1].keys) >= self.t:
+            self.borrow_from_next(idx)
+        else:
+            if idx != len(self.keys):
+                self.merge(idx)
+            else:
+                self.merge(idx - 1)
+
+    def borrow_from_prev(self, idx):
+        child = self.children[idx]
+        sibling = self.children[idx - 1]
+
+        child.keys.insert(0, self.keys[idx - 1])
+        if not child.leaf:
+            child.children.insert(0, sibling.children.pop())
+        self.keys[idx - 1] = sibling.keys.pop()
+
+    def borrow_from_next(self, idx):
+        child = self.children[idx]
+        sibling = self.children[idx + 1]
+
+        child.keys.append(self.keys[idx])
+        if not child.leaf:
+            child.children.append(sibling.children.pop(0))
+        self.keys[idx] = sibling.keys.pop(0)
+
+    def merge(self, idx):
+        child = self.children[idx]
+        sibling = self.children[idx + 1]
+
+        child.keys.append(self.keys.pop(idx))
+        child.keys.extend(sibling.keys)
+
+        if not child.leaf:
+            child.children.extend(sibling.children)
+
+        self.children.pop(idx + 1)
 
 
 class BTree:
     def __init__(self, t):
-        self.root = BTreeNode(True)
         self.t = t
+        self.root = BTreeNode(t, leaf=True)
 
-    # Insert a key
+    def search(self, k):
+        return self.root.search(k)
+
     def insert(self, k):
-        root = self.root
-        if len(root.keys) == (2 * self.t) - 1:
-            temp = BTreeNode()
-            self.root = temp
-            temp.child.insert(0, root)
-            self.split_child(temp, 0)
-            self.insert_non_full(temp, k)
+        r = self.root
+        if len(r.keys) == 2 * self.t - 1:
+            s = BTreeNode(self.t, leaf=False)
+            s.children.append(r)
+            self.root = s
+            s.split_child(0, r)
+            s.insert_non_full(k)
         else:
-            self.insert_non_full(root, k)
+            r.insert_non_full(k)
 
-    # Insert non full
-    def insert_non_full(self, x, k):
-        i = len(x.keys) - 1
-        if x.leaf:
-            x.keys.append((None, None))
-            while i >= 0 and k[0] < x.keys[i][0]:
-                x.keys[i + 1] = x.keys[i]
-                i -= 1
-            x.keys[i + 1] = k
-        else:
-            while i >= 0 and k[0] < x.keys[i][0]:
-                i -= 1
-            i += 1
-            if len(x.child[i].keys) == (2 * self.t) - 1:
-                self.split_child(x, i)
-                if k[0] > x.keys[i][0]:
-                    i += 1
-            self.insert_non_full(x.child[i], k)
+    def traverse(self, node=None, level=0):
+        if node is None:
+            node = self.root
 
-    # Split the child
-    def split_child(self, x, i):
-        t = self.t
-        y = x.child[i]
-        z = BTreeNode(y.leaf)
-        x.child.insert(i + 1, z)
-        x.keys.insert(i, y.keys[t - 1])
-        z.keys = y.keys[t: (2 * t) - 1]
-        y.keys = y.keys[0: t - 1]
-        if not y.leaf:
-            z.child = y.child[t: 2 * t]
-            y.child = y.child[0: t - 1]
+        print("Level", level, " ", len(node.keys), "keys:", node.keys)
+        if not node.leaf:
+            for child in node.children:
+                self.traverse(child, level + 1)
 
-    # Delete a node
-    def delete(self, x, k):
-        t = self.t
-        i = 0
-        while i < len(x.keys) and k[0] > x.keys[i][0]:
-            i += 1
-        if x.leaf:
-            if i < len(x.keys) and x.keys[i][0] == k[0]:
-                x.keys.pop(i)
-                return
-            return
 
-        if i < len(x.keys) and x.keys[i][0] == k[0]:
-            return self.delete_internal_node(x, k, i)
-        elif len(x.child[i].keys) >= t:
-            self.delete(x.child[i], k)
-        else:
-            if i != 0 and i + 2 < len(x.child):
-                if len(x.child[i - 1].keys) >= t:
-                    self.delete_sibling(x, i, i - 1)
-                elif len(x.child[i + 1].keys) >= t:
-                    self.delete_sibling(x, i, i + 1)
-                else:
-                    self.delete_merge(x, i, i + 1)
-            elif i == 0:
-                if len(x.child[i + 1].keys) >= t:
-                    self.delete_sibling(x, i, i + 1)
-                else:
-                    self.delete_merge(x, i, i + 1)
-            elif i + 1 == len(x.child):
-                if len(x.child[i - 1].keys) >= t:
-                    self.delete_sibling(x, i, i - 1)
-                else:
-                    self.delete_merge(x, i, i - 1)
-            self.delete(x.child[i], k)
-
-    # Delete internal node
-    def delete_internal_node(self, x, k, i):
-        t = self.t
-        if x.leaf:
-            if x.keys[i][0] == k[0]:
-                x.keys.pop(i)
-                return
-            return
-
-        if len(x.child[i].keys) >= t:
-            x.keys[i] = self.delete_predecessor(x.child[i])
-            return
-        elif len(x.child[i + 1].keys) >= t:
-            x.keys[i] = self.delete_successor(x.child[i + 1])
-            return
-        else:
-            self.delete_merge(x, i, i + 1)
-            self.delete_internal_node(x.child[i], k, self.t - 1)
-
-    # Delete the predecessor
-    def delete_predecessor(self, x):
-        if x.leaf:
-            return x.pop()
-        n = len(x.keys) - 1
-        if len(x.child[n].keys) >= self.t:
-            self.delete_sibling(x, n + 1, n)
-        else:
-            self.delete_merge(x, n, n + 1)
-        self.delete_predecessor(x.child[n])
-
-    # Delete the successor
-    def delete_successor(self, x):
-        if x.leaf:
-            return x.keys.pop(0)
-        if len(x.child[1].keys) >= self.t:
-            self.delete_sibling(x, 0, 1)
-        else:
-            self.delete_merge(x, 0, 1)
-        self.delete_successor(x.child[0])
-
-    # Delete resolution
-    def delete_merge(self, x, i, j):
-        cnode = x.child[i]
-
-        if j > i:
-            rsnode = x.child[j]
-            cnode.keys.append(x.keys[i])
-            for k in range(len(rsnode.keys)):
-                cnode.keys.append(rsnode.keys[k])
-                if len(rsnode.child) > 0:
-                    cnode.child.append(rsnode.child[k])
-            if len(rsnode.child) > 0:
-                cnode.child.append(rsnode.child.pop())
-            new = cnode
-            x.keys.pop(i)
-            x.child.pop(j)
-        else:
-            lsnode = x.child[j]
-            lsnode.keys.append(x.keys[j])
-            for i in range(len(cnode.keys)):
-                lsnode.keys.append(cnode.keys[i])
-                if len(lsnode.child) > 0:
-                    lsnode.child.append(cnode.child[i])
-            if len(lsnode.child) > 0:
-                lsnode.child.append(cnode.child.pop())
-            new = lsnode
-            x.keys.pop(j)
-            x.child.pop(i)
-
-        if x == self.root and len(x.keys) == 0:
-            self.root = new
-
-    # Delete the sibling
-    def delete_sibling(self, x, i, j):
-        cnode = x.child[i]
-        if i < j:
-            rsnode = x.child[j]
-            cnode.keys.append(x.keys[i])
-            x.keys[i] = rsnode.keys[0]
-            if len(rsnode.child) > 0:
-                cnode.child.append(rsnode.child[0])
-                rsnode.child.pop(0)
-            rsnode.keys.pop(0)
-        else:
-            lsnode = x.child[j]
-            cnode.keys.insert(0, x.keys[i - 1])
-            x.keys[i - 1] = lsnode.keys.pop()
-            if len(lsnode.child) > 0:
-                cnode.child.insert(0, lsnode.child.pop())
-
-    # Print the tree
-    def print_tree(self, x, l=0):
-        print("Level ", l, " ", len(x.keys), end=":")
-        for i in x.keys:
-            print(i, end=" ")
-        print()
-        l += 1
-        if len(x.child) > 0:
-            for i in x.child:
-                self.print_tree(i, l)
--- src/Main.py
+++ src/Main.py
@@ -3,10 +3,56 @@ Created on Dec 10, 2025
 
 @author: jacksonhooper
 '''
-
+from BTree import BTree
 import csv
 import tkinter as tk
 from tkinter import filedialog
+from pickle import TRUE, FALSE
+
+
+#
+# Helper function to determine whether a field can be indexed or not
+#
+def check_double(s: str) -> bool:
+    try:
+        
+        float(s)
+        return True
+    
+    except ValueError:
+        
+        try:
+            
+            float(s[1:])
+            return True
+        
+        except:
+            
+            return False
+
+#
+# Changes given string into a double for BTree implementation
+#
+def make_double(str):
+    
+    num = "1234567890"
+    return_str = ""
+    has_decimal = False
+    
+    for letter in str:
+        
+        if letter == ".":
+            
+            if not has_decimal:
+                
+                has_decimal = True
+                return_str += "."
+        
+        elif letter in num:
+            
+            return_str += letter
+    
+    return float(return_str)
 
 
 #
@@ -14,7 +60,7 @@ from tkinter import filedialog
 #
 def prompt_user_for_file():
     
-    print("Submit CSV File:")
+    print("Select a CSV File:")
     root = tk.Tk()
     root.withdraw() # Hide the main tkinter window
     file_path = filedialog.askopenfilename(
@@ -28,7 +74,7 @@ def prompt_user_for_file():
 # Calls prompt_user_for_file to obtain CSV file. Accounts for non-submissions and errors in reading the file
 # Sorts the CSV file into a list of lists.
 #
-def fileSort():
+def file_organizer():
     
     selected_file = prompt_user_for_file()
     
@@ -72,19 +118,225 @@ def fileSort():
         
         print("No file selected.")
         
+#
+# Prints all field headers along with their index to allow for choosing individual fields
+#    
+def print_fields(fields):
+    
+    if(len(fields) > 0):
+        
+        print("\nChoose a field:")
+        
+        for i in range(len(fields)):
+            
+            print(f"{i} - {fields[i]}")
+        
+        print(f"{i + 1} - QUIT PROGRAM")
+        
+    else:
+        
+        print("CSV File has no fields")
 
+#
+# Prints all base case options for the user
+#
+def print_user_options():
+    
+    print("\nChoose an Action:")
+    print("1 - Create Index")
+    print("2 - Exact Value Search")
+    print("3 - Range Query")
+    print("4 - Close Program")
+    return input()
+
+#
+# Sets arrays that store states and locations of hash tables and index state for each column
+#
+def set_hash_and_index(file):
+    
+    
+    hash = []
+    index = []
+    
+    for i in range(len(file[0])):
+        
+        hash.append(create_hash_table(i, file))
+        index.append([False, None])
+    
+    return hash, index
+
+#
+# Creates hash table for given column
+#
+def create_hash_table(column, file):
+    
+    #Multiplies by 1.5 to allow for sufficient space within hash table
+    hash_array = [-1] * next_prime(int(len(file) * 1.5))
+    
+    for i in range(len(file)):
+        
+        if i != 0:
+        
+            hash_array[hash_function(str(file[i][column]), len(hash_array))] = file[i][column]
+    
+    return hash_array
+  
+#
+# Returns next prime greater than given int
+#  
+def next_prime(n: int) -> int:
+    
+    def is_prime(x: int) -> bool:
+        
+        if(x < 2):      return False
+        if(x == 2):     return True
+        if(x % 2 == 0): return False
+        
+        for i in range(3, int(x ** 0.5) + 1, 2):
+            
+            if x % i == 0: return False
+            
+        return True
+
+    candidate = n + 1
+    while True:
+        if is_prime(candidate):
+            return candidate
+        candidate += 1
+        
+#
+# Utilizes my hash function from HW5 to create hash values
+#
+def hash_function(str, size):
+    
+    p = 53
+    m = size
+    hash_value = 0
+    p_pow = 1
+    
+    for c in str:
+        
+        hash_value = (hash_value + (ord(c) + 1) * p_pow) % m
+        p_pow = (p_pow * p) % m
+        
+    return int(hash_value);
+
+#
+# Creates a new index
+#
+def create_index(index_array, file):
+    
+    indexes_left = False
+    valid_choices = []
+    counter = 0;
+    
+    for bool in index_array:
+        
+        if not bool[0]:
+            
+            indexes_left = True
+    
+    
+    if indexes_left:
+    
+        print("These fields are indexed:")
+        
+        for i in range(len(index_array)):
+            
+            if index_array[i][0]:
+                
+                print(f"{file[0][i]}")
+                
+        print("Which field would you like to index?")
+        
+        for i in range(len(file[0])):
+            
+            if not index_array[i][0]:
+                
+                if check_double(file[1][i]):
+                    
+                    valid_choices.append(str(i))
+                    print(f"{i} - {file[0][i]}")
+        
+        user_index_choice = input()
+        
+        try:
+            
+            if valid_choices.index(user_index_choice) == -1:
+                    
+                print("Not a Valid Choice")
+                return index_array
+            
+            else:
+                    
+                index_array[int(user_index_choice)] = [True, create_b_tree(int(user_index_choice), file)]
+                return index_array
+        
+        except:
+            
+            print("Not a Valid Choice")
+            return index_array
+            
+    
+    else:
+        
+        print("All fields are indexed")
+        return index_array
+
+
+#
+# Creates a b-tree
+#
+def create_b_tree(index, file):
+    
+    tree = BTree(5)
+    counter = 0
+    
+    for row in file:
+        
+        if counter != 0:
+        
+            tree.insert(make_double(row[index]))
+            
+        counter += 1
+        
+    return tree
 #
 # Calls fileSort to obtain CSV File. Currently just ensuring the list of lists is being created properly.
 #
 def main():
     
-    inputFile = fileSort()
+    #prompts CSV file upload
+    inputFile = file_organizer()
     
-    print(inputFile[0])
-    print(inputFile[1])
-    print(inputFile[2])
-    print(inputFile[3])
-    print(inputFile[4])
+    #create hash tables and index array
+    hash_tables, indexed_fields = set_hash_and_index(inputFile)
+    
+    #runs until user submits something that is not a 1 2 or 3
+    while True:
+        
+        user_choice = print_user_options()
+        
+        if user_choice == "1":
+        
+            indexed_fields = create_index(indexed_fields, inputFile)
+            
+            for range in indexed_fields:
+                print(range)
+            
+        if user_choice == "2":
+        
+            pass
+        
+        if user_choice == "3":
+            
+            pass
+            
+        if user_choice == "4":
+            
+            break
+    
+    
         
 
 if __name__ == '__main__':
